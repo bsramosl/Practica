dfd25a7f6c6eaee7216a4c626653ff9e6bd92a69
Lopez <steeven10f@gmail.com>
2022-03-31 12:22:08 -0500

    FULLsTA

---
+++ Basico.py
@@ -0,0 +1,26 @@
+#imprimir -- Strings
+from calendar import c
+from this import d
+
+
+print ("Hola Mundo")
+print ("hola mundo"+"comenzamos")
+
+# Numeros 
+
+a = 5
+b = 5.10
+c = 6
+d = 3.5
+#Suma
+print (a+b)
+#Resta
+print(a-c)
+#Multiplicacion
+print(a*b)
+#Division
+print(b/d)
+#Exponente
+print(c**2)
+#Modulo
+print( 7%3 )
